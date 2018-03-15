69d1696e517a85d339a608f6d9144361deb2f487
Kelly Kong <kellykong2772@gmail.com>
2018-03-16 00:14:14 +0800

    bus appear with route

--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -1,5 +1,5 @@
+//generates the google map
 import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { AlertController } from 'ionic-angular';
 import {
     GoogleMaps,
     GoogleMap,
@@ -10,7 +10,6 @@ import {
     Marker,
     Spherical
 } from '@ionic-native/google-maps';
-import { BusLocationProvider } from '../../providers/bus-location/bus-location';
 import { Observable } from 'rxjs/Observable';
 import { RouteProvider } from '../../providers/route/route';
 import { MapProvider } from '../../providers/map/map';
@@ -33,41 +32,18 @@ import { RouteComponent } from '../route/route';
      public map: GoogleMap;
      @ViewChild('map') mapElement: ElementRef;
      private trafficFlag:boolean = false;
-     public buses = new Array();
-     private allMarkers = new Array();
+     
      public allRoutes = [];
 
      constructor(
          public googleMaps: GoogleMaps,
-         public alertCtrl: AlertController,
-         public busLocationProvider: BusLocationProvider,
-         public spherical: Spherical,
          public routeProvider: RouteProvider,
          public mapProvider: MapProvider
          ) {
          console.log('Hello MapComponent Component');
-         this.getBuses();
      }
 
      ngOnInit(){
-         console.log('const', this.buses);
-         let myObservable = Observable.create(observer => {
-             setInterval(() => {
-                 this.getBuses();
-                // bus markers
-                if(this.buses){
-                    observer.next(this.buses);
-                }
-            },2000);
-         });
-
-         myObservable.subscribe((data) => {
-             if(this.buses){
-                 for(var i = 0; i < this.buses.length; i++){
-                     this.updateMarker(this.buses[i]);
-                 }
-             };
-         });
          this.loadMap();
      }
 
@@ -110,49 +86,4 @@ import { RouteComponent } from '../route/route';
         this.mapProvider.map.setTrafficEnabled(this.trafficFlag);
     }
 
-    addBusMarker(bus){
-        // var SlidingMarker = require('marker-animate-unobtrusive');
-        let m = this.mapProvider.map.addMarker({
-            position: JSON.parse(bus.bus_location),
-            icon: {url: './assets/icon/bus.png', size: {width: 35, height: 45}},
-        });
-        this.allMarkers.push(m);
-    }
-
-    updateMarker(bus){
-        for(var i = 0; i < this.allMarkers.length; i++){
-            if(i +1 == bus.id){
-                let location = JSON.parse(bus.bus_location);
-                let track_status = bus.track_status
-                //if location is null and track_status === 'OFF', delete marker,
-                //else update marker position
-                if(location == null && track_status){
-                    this.allMarkers[i].then(marker => {
-                        marker.remove();
-                    });
-                    this.allMarkers.splice(i, 1);
-                    console.log(this.allMarkers);
-                }
-                else{
-                    this.allMarkers[i].then(marker =>{
-                        var newPosition = JSON.parse(bus.bus_location);
-                        // marker.setPosition(newPosition);
-                        //this.spherical.interpolate(originalPosition, newPosition, fraction); fraction means how many percent the marker should go.
-                        marker.setPosition(this.spherical.interpolate(marker.getPosition(), newPosition, 1.0));
-                    });
-                }
-                return;
-            }
-        }
-        //add Marker
-        this.addBusMarker(bus);
-    }
-
-    getBuses(){
-        this.busLocationProvider.getLocationService().subscribe((res) => {
-            this.buses = res;
-        }, (err) => {
-            alert('something is wrong! ' + err);
-        });
-    }
 }
--- src/components/route/route.ts
+++ src/components/route/route.ts
@@ -1,12 +1,14 @@
+//this component deals with the route polyline and bus respectively to the route
 import { Component } from '@angular/core';
 import { RouteProvider } from '../../providers/route/route';
-// import { BusStopProvider } from '../../providers/bus-stop/bus-stop';
 import { MapProvider } from '../../providers/map/map';
 import { Observable } from 'rxjs/Observable';
 import { AlertController } from 'ionic-angular';
 import {
     GoogleMapsEvent,
+    Spherical
 } from '@ionic-native/google-maps';
+import { BusLocationProvider } from '../../providers/bus-location/bus-location';
 /**
  * Generated class for the RouteComponent component.
  *
@@ -19,18 +21,28 @@ import {
  })
 export class RouteComponent {
 
+    //variables for bus stops marker and polylines
     public routeArr = [];
     private testCheckboxResult = [true, false, false, false];
     private testCheckboxOpen: boolean;
     public allPoly = [];
     public polyList = [];
-    public markerList = [];
+    public busStopMarkerList = [];
+
+    //variables for buses
+    public allBusMarkers = new Array();
+    public buses = new Array();
+    public selectedRoute:any;
+
+    public myObservable:any;
+    public obsListener = [];
 
     constructor(
-        // public busStopProvider: BusStopProvider,
         public routeProvider: RouteProvider,
         public mapProvider: MapProvider,
-        public alertCtrl: AlertController
+        public alertCtrl: AlertController,
+        public busLocationProvider: BusLocationProvider,
+        public spherical: Spherical,
     ) {
         console.log('Hello RouteComponent Component');
         this.getRoute();
@@ -40,7 +52,17 @@ export class RouteComponent {
         setTimeout(() => {
             this.routeProvider.setSelectedRoute([this.routeArr[0].id]); //show first route on map on app start up
             this.showRoutes();
-         }, 2000);
+        }, 2000);
+
+        this.myObservable = Observable.create(observer => {
+            setInterval(() => {
+                this.getBuses();
+                // bus markers
+                if(this.buses){
+                    observer.next(this.buses);
+                }
+            },2000);
+        });
     }
 
         //alert for bus filter
@@ -69,7 +91,7 @@ export class RouteComponent {
             text: 'Okay',
             handler: data => {
                 this.testCheckboxOpen = false;
-                this.removePolylines();
+                this.resetRoutes();
                 this.routeProvider.setSelectedRoute(data);
                 this.showRoutes();
             }
@@ -95,6 +117,7 @@ export class RouteComponent {
         for(var i = 0; i < selectedID.length; i++){
             for(var j = 0; j < this.routeArr.length; j++){
                 if(selectedID[i] == this.routeArr[j].id){ //if match, then show polyline on map
+                    //add route polyline
                     let polyline = JSON.parse(this.routeArr[j].polyline);
                     let color = this.routeArr[j].color;
                     console.log('color', color);
@@ -115,24 +138,106 @@ export class RouteComponent {
                             title: stop.name,
                             icon: {url: './assets/icon/bus-stop.png', size: {width: 15, height: 15}},
                         }).then(m => {
-                            this.markerList.push(m);
+                            this.busStopMarkerList.push(m);
                         });
-                    })
+                    });
+                    //add bus marker
+                    this.generateBusMarkerOnMap(selectedID[i]);
                 };
             };
         };
     }
 
-    removePolylines(){
+    resetRoutes(){
         this.polyList.forEach(p => {
             p.remove();
         });
         this.polyList = []; //initialize polyline list
 
-        this.markerList.forEach(m => {
+        this.busStopMarkerList.forEach(m => {
             m.remove();
         });
-        this.markerList = []; //initialize marker list
+        this.busStopMarkerList = []; //initialize marker list
+
+        this.allBusMarkers.forEach((bus) => { //remove all created bus marker
+            bus.then(marker => {
+                marker.remove();
+            });
+        });
+        this.allBusMarkers = [];
+
+        this.resetObs();
     }
 
+    //========================================================================================
+    //deals with the bus marker
+    getBuses(){
+        this.busLocationProvider.getLocationService().subscribe((res) => {
+            this.buses = res;
+        }, (err) => {
+            // this.resetObs();
+            console.log('something is wrong! ' + err); //KIV: always internal server error
+        });
+    }
+
+    generateBusMarkerOnMap(selectedRoute){
+        let o = this.myObservable.subscribe((data) => {
+            data.forEach(d => {
+                console.log('selectedR', selectedRoute);
+                console.log('d', d.route_id);
+                if(selectedRoute == d.route_id){
+                    this.updateBusMarker(d);
+                }
+            });
+        });
+
+        this.obsListener.push(o); //push observables into array
+    };
+
+    resetObs(){
+        this.obsListener.forEach(o => {
+            o.unsubscribe();
+        });
+        this.obsListener = [];
+    }
+
+    addBusMarker(bus){
+        // var SlidingMarker = require('marker-animate-unobtrusive');
+        let m = this.mapProvider.map.addMarker({
+            position: JSON.parse(bus.bus_location),
+            icon: {url: './assets/icon/bus.png', size: {width: 35, height: 45}},
+            subtitle: bus.bus_number
+        });
+        this.allBusMarkers.push(m);
+    }
+
+    updateBusMarker(bus){
+        console.log('update bus', bus);
+        for(var i = 0; i < this.allBusMarkers.length; i++){
+            if(i +1 == bus.id){
+                let location = JSON.parse(bus.bus_location);
+                let track_status = bus.track_status
+                //if location is null and track_status === 'OFF', delete marker,
+                //else update marker position
+                if(location == null && track_status){
+                    this.allBusMarkers[i].then(marker => {
+                        marker.remove();
+                    });
+                    this.allBusMarkers.splice(i, 1);
+                    console.log(this.allBusMarkers);
+                }
+                else{
+                    this.allBusMarkers[i].then(marker =>{
+                        var newPosition = JSON.parse(bus.bus_location);
+                        // marker.setPosition(newPosition);
+                        //this.spherical.interpolate(originalPosition, newPosition, fraction); fraction means how many percent the marker should go.
+                        marker.setPosition(this.spherical.interpolate(marker.getPosition(), newPosition, 1.0));
+                    });
+                }
+                return;
+            }
+        }
+        //add Marker
+        this.addBusMarker(bus);
+    }
 }
