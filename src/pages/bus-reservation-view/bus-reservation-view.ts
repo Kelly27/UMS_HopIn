41e7cbd154c7f1ab04f3fe0cf73feafbc87d114c
Kelly Kong <kellykong2772@gmail.com>
2018-04-27 22:58:46 +0800

    done view

--- src/pages/bus-reservation-view/bus-reservation-view.ts
+++ src/pages/bus-reservation-view/bus-reservation-view.ts
@@ -11,6 +11,8 @@ import { GoogleMaps, GoogleMap, GoogleMapOptions, GoogleMapsEvent } from '@ionic
 export class BusReservationViewPage {
   public map: GoogleMap;
   @ViewChild('map') mapElement: ElementRef;
+  public map2: GoogleMap;
+  @ViewChild('map2') map2Element: ElementRef;
 
   public reserve = {};
   public applicant_info = {};
@@ -40,13 +42,30 @@ export class BusReservationViewPage {
       }
     }
 
+    let map2Options: GoogleMapOptions = {
+      camera: {
+        zoom: 15,
+        target: JSON.parse(this.navParams.get('reservation').drop_off_location)
+      }
+    }
+
     let element = this.mapElement.nativeElement;
     this.map = this.googleMaps.create(element, mapOptions);
+
+    let element2 = this.map2Element.nativeElement;
+    this.map2 = this.googleMaps.create(element2, map2Options);
 
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
       console.log('map is ready!');
       this.map.addMarker({
         position: JSON.parse(this.navParams.get('reservation').pick_up_location),
+      })
+    });
+
+    this.map2.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log('map is ready!');
+      this.map2.addMarker({
+        position: JSON.parse(this.navParams.get('reservation').drop_off_location),
       })
     });
 
